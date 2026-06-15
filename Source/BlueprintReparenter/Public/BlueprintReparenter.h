d79a91025e7ae8f183117c0cc37519f61768255d
Oliver Iking <13166781+zsoi@users.noreply.github.com>
2026-06-15 11:07:15 +0200

    Added support for multi-selection. Only shows up if all selected assets are blueprints. No checks for viability of reparenting was added, still use at your own discretion

--- Source/BlueprintReparenter/Public/BlueprintReparenter.h
+++ Source/BlueprintReparenter/Public/BlueprintReparenter.h
@@ -12,7 +12,7 @@ public:
 private:
 	void RegisterMenuExtender();
 	TSharedRef<class FExtender> OnExtendContentBrowserMenu(const TArray<FAssetData>& SelectedAssets);
-	void AddReparentOption(class FMenuBuilder& MenuBuilder, FAssetData SelectedAsset);
-	void OnReparentBlueprint(FAssetData BlueprintAsset);
+	void AddReparentOption(class FMenuBuilder& MenuBuilder, TArray<FAssetData> SelectedAssets);
+	void OnReparentBlueprints(TArray<FAssetData> BlueprintAssets);
 	static TSharedRef<FExtender> OnExtendContentBrowserMenu_Static(const TArray<FAssetData>& SelectedAssets);
 };
